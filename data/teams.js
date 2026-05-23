ed7f4c0b9fe835df901a2b82a6058e8d31879856
tuzcupalamut <tuzcupalamut@gmail.com>
2026-05-24 00:45:52 +0300

    guncelleme

--- data/teams.js
+++ data/teams.js
@@ -27,6 +27,36 @@ emoji: "1507473236630442124"
 },
 
 {
+label: "Erzurumspor FK",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Kocaelispor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Konyaspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Göztepe SK",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Çaykur Rizespor",
+roleId: "1507473816375529472",
+emoji: "1507473011031277658"
+},
+
+{
 label: "Gaziantep FK",
 roleId: "1507473708296441936",
 emoji: "1507473051925614685"
@@ -43,15 +73,75 @@ emoji: "1504772484879352058"
 },
 
 {
-label: "Sakaryaspor",
-roleId: "1507473818212503603",
-emoji: "1507473106376331306"
+label: "Gençlerbirliği",
+roleId: "",
+emoji: ""
 },
 
 {
-label: "Çaykur Rizespor",
-roleId: "1507473816375529472",
-emoji: "1507473011031277658"
+label: "Manisa FK",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Vanspor FK",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Mardin 1969 Spor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Çorum FK",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Muğlaspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Iğdır FK",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Batman Petrolspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Boluspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Kayserispor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Sivasspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "İstanbulspor",
+roleId: "",
+emoji: ""
 }
 
 ],
@@ -59,13 +149,103 @@ emoji: "1507473011031277658"
 lig2: [
 
 {
-label: "Eskişehirspor",
-roleId: "1507473938907922584",
-emoji: "1507473029389750434"
+label: "68 Aksaray Belediyespor",
+roleId: "",
+emoji: ""
 },
 
 {
-label: "Kastamonuspor",
+label: "Karaman FK",
+roleId: "1507474127588556960",
+emoji: "1507473092237328534"
+},
+
+{
+label: "Sakaryaspor",
+roleId: "1507473818212503603",
+emoji: "1507473106376331306"
+},
+
+{
+label: "12 Bingölspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Muşspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Şanlıurfaspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Belediye Kütahyaspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Kahramanmaraş İstiklalspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Hatayspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "MKE Ankaragücü",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Elazığspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Kırklarelispor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Isparta 32 Spor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "24 Erzincanspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "52 Orduspor FK",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Adanaspor",
+roleId: "1507835691596382418",
+emoji: "1507835955288080415"
+},
+
+{
+label: "GMG Kastamonuspor",
 roleId: "1507474003319717918",
 emoji: "1507473074172461187"
 }
@@ -75,9 +255,111 @@ emoji: "1507473074172461187"
 lig3: [
 
 {
-label: "Karaman FK",
-roleId: "1507474127588556960",
-emoji: "1507473092237328534"
+label: "Eskişehirspor",
+roleId: "1507473938907922584",
+emoji: "1507473029389750434"
+},
+
+{
+label: "Amasyaspor FK",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Tokat Belediye Plevnespor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Osmaniyespor FK",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Yozgat Belediyesi Bozok Spor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Denizli İdmanyurdu Güreller SK",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Yeni Mersin İdmanyurdu",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Balıkesirspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Niğde Belediyespor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Diyarbekirspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Kırıkkale FK",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Düzcespor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Karabük İdmanyurdu",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Ağrı 1970 Spor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Kırşehir FK",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Uşakspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Yeni Malatyaspor",
+roleId: "",
+emoji: ""
+},
+
+{
+label: "Zonguldak Kömürspor",
+roleId: "",
+emoji: ""
 },
 
 {
